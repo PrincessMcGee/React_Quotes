70405bb0f3073d3d14bdca58362a610340fa79dc
PrincessMcGee <60694770+PrincessMcGee@users.noreply.github.com>
2020-02-21 09:54:38 +0100

    Added components and edited in them
    Added components and edited in them

---
+++ src/components/News.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const News = (props) => {
+    return(
+        <div className="container">
+            <h4 className="center">News from Cute Quotes</h4>
+            <p>News will be featured here!</p>
+        </div>
+    )
+}
+
+export default News
