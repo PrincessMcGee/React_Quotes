a0ff91d6e7d89d9ed6394fe3c0bbc714eb8a19eb
PrincessMcGee <60694770+PrincessMcGee@users.noreply.github.com>
2020-02-21 12:12:58 +0100

    Edited
    Edited

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import Contact from './components/Contact';
 // import Home from './components/Home';
-
+import AddQuote from './AddQuote';
 
 class Quote_App extends Component {
+  state = {
+    quotes : [
+      {title: 'Getting started', author: 'Nelson Mandela', citetext: 'The way to get started is to quit talking and start doing.', id: 1},
+      {title: 'Light and dark', author: 'Aristotle', citetext: 'It is during our darkest moments that we must focus to see the light.', id: 2},
+      {title: 'The best and most beautiful things', author: 'Helen Keller', citetext: 'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.', id: 3},
+      {title: 'Defeats', author: 'Maya Angelou', citetext: 'You will face many defeats in life, but never let yourself be defeated.', id: 4},
+      {title: 'What life is', author: 'Helen Keller', citetext: 'Life is either a daring adventure or nothing at all.', id: 5}
+    ]
+  }
+  addQuote = (quote) => {
+    quote.id = Math.random();
+    let quotes = [...this.state.quotes, quote]
+    this.setState({
+      quotes: quotes
+    })
+  }
   render(){
     return (
       <BrowserRouter>
@@ -14,12 +30,11 @@ class Quote_App extends Component {
         <Navbar />
         <Route path='/about' component={About} />
         <Route path='/contact' component={Contact} />
-
+        <Quotes quotes={this.state.quotes} />
+        <AddQuote addQuote={this.addQuote} />
       </div>
       </BrowserRouter>
     );
   }
-  
 }
-
-export default Quote_App;
+export default Quote_App
